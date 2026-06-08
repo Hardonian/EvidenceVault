e4c95ddc4dd0b88bcc9ac375073a5a38a6c09154
Scott Hardie <scottrmhardie@gmail.com>
2026-06-08 00:25:25 -0400

    ⚡ Optimize memory allocations and iteration in evidence.All (#42)

--- internal/evidence/service.go
+++ internal/evidence/service.go
@@ -222,11 +222,16 @@ func (s *Service) Files(tenantID string) []File {
 	})
 	return out
 }
-func (s *Service) All() []Item {
+func (s *Service) All(filter func(Item) bool) []Item {
 	out := []Item{}
 	_ = s.store.Read(func(st *persistence.State) error {
 		for _, arr := range st.Evidence {
-			out = append(out, toItems(arr)...)
+			for _, persistItem := range arr {
+				it := Item(persistItem)
+				if filter == nil || filter(it) {
+					out = append(out, it)
+				}
+			}
 		}
 		return nil
 	})
